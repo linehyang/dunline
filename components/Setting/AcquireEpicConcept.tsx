127872b2a1a2e5c96cb3e84249ddad4832fa601f
felix-watcha <felix@watcha.com>
2022-04-25 02:33:40 +0900

    refactor: epic item modeling

--- components/Setting/AcquireEpicConcept.tsx
+++ components/Setting/AcquireEpicConcept.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import useSWR from "swr";
 import Image from "next/image";
-import { Box, Tooltip } from "@chakra-ui/react";
+import { Box, Tooltip, Text } from "@chakra-ui/react";
 
-import { Concept, EpicConcept } from "../../public/epicConcept";
-import { EpicInfo, EpicInfoEquip } from "../../public/epicInfo";
+import { EpicConcept, EpicItems } from "../../public/epic";
+
 import HoverEpicInfo from "./HoverEpicInfo";
 import ConceptFilter from "./ConceptFilter";
 
@@ -12,30 +12,6 @@ type Props = {
   server: string | string[] | undefined;
   characterid: string | string[] | undefined;
 };
-
-// 컨셉 영어를 한글로 바꿔주는 함수
-function conCeptFilter(concept: string) {
-  let result: string = "";
-  Object.entries(EpicConcept).map(([key, value]) => {
-    if (key === concept) {
-      result = value;
-    }
-  });
-  return result;
-}
-
-//특정 에픽이 어느부위인지 알려주는 함수
-function equipFilter(equip: string) {
-  let result: string = "";
-  Object.entries(EpicInfo).map(([item, itemObject]) => {
-    if (Object.prototype.hasOwnProperty.call(itemObject, equip)) {
-      result = item;
-      return item;
-    }
-  });
-
-  return result;
-}
 
 type EpicConceptKeyType = keyof typeof EpicConcept;
 
@@ -65,54 +41,40 @@ export default function AcquireEpicConcept({ server, characterid }: Props) {
         />
       ) : null}
       {data
-        ? Object.entries(Concept)
-            .filter(([concept]) =>
-              concepts.includes(concept as EpicConceptKeyType)
-            )
-            .map(([concetp, concetpEpic]) => {
-              return (
-                <>
-                  <Box>
-                    {conCeptFilter(concetp)}
-                    <Box display={"flex"} flexWrap={"wrap"}>
-                      {Object.entries(concetpEpic).map(([itemId, itemName]) => {
-                        return (
-                          <Tooltip
-                            label={<HoverEpicInfo itemId={itemId} />}
-                            fontSize="md"
-                            key={`${conCeptFilter(concetp)}${itemName}`}
-                          >
-                            <Box
-                              filter={
-                                data.includes(itemId)
-                                  ? "grayscale(0%)"
-                                  : "grayscale(100%)"
-                              }
-                            >
-                              <Box fontSize={"2px"} textAlign={"center"}>
-                                {
-                                  EpicInfoEquip[
-                                    equipFilter(
-                                      itemId
-                                    ) as keyof typeof EpicInfoEquip
-                                  ]
-                                }
-                              </Box>
-                              <Image
-                                src={`https://img-api.neople.co.kr/df/items/${itemId}`}
-                                alt={"에픽아이템"}
-                                width={"30px"}
-                                height={"30px"}
-                              />
-                            </Box>
-                          </Tooltip>
-                        );
-                      })}
+        ? concepts.map((concept) => (
+            <Box key={concept}>
+              <Text>{EpicConcept[concept]}</Text>
+              <Box display={"flex"} flexWrap={"wrap"}>
+                {EpicItems.filter((epicItem) =>
+                  epicItem.concepts?.includes(concept)
+                ).map(({ itemId, itemName, parts }) => (
+                  <Tooltip
+                    label={<HoverEpicInfo itemId={itemId} />}
+                    fontSize="md"
+                    key={`${EpicConcept[concept]}${itemName}`}
+                  >
+                    <Box
+                      filter={
+                        data.includes(itemId)
+                          ? "grayscale(0%)"
+                          : "grayscale(100%)"
+                      }
+                    >
+                      <Box fontSize={"8px"} textAlign={"center"}>
+                        {parts}
+                      </Box>
+                      <Image
+                        src={`https://img-api.neople.co.kr/df/items/${itemId}`}
+                        alt={"에픽아이템"}
+                        width={"30px"}
+                        height={"30px"}
+                      />
                     </Box>
-                  </Box>
-                </>
-              );
-            })
+                  </Tooltip>
+                ))}
+              </Box>
+            </Box>
+          ))
         : null}
     </>
   );
