c8cac9291e7002012f181d0d2fd62e848adfb7f9
linehyang <aldzkwp1912@gmail.com>
2022-07-25 01:47:36 +0900

    add ispins epic

--- scripts/getEpicInfo.mjs
+++ scripts/getEpicInfo.mjs
@@ -264,43 +264,17 @@ const all = [
   "딥 다이버 오리발",
   "완성형 동력 제어장치",
   "딥 다이버 마스크",
-];
-const shinwha = [
-  "대제사장의 예복", //
-  "대 마법사 [???]의 로브", //
-  "낭만적인 선율의 왈츠", //
-  "고대 심연의 로브", //
-  "생사를 다스리는 그림자의 재킷", //
-  "수석 집행관의 코트", //
-  "최후의 전술", //
-  "길 방랑자의 물소 코트", //
-  "작열하는 대지의 용맹", //
-  "트로피카 : 드레이크", //
-  "웨어러블 아크 팩", //
-  "종말의 역전", //
-  "사탄 : 분노의 군주", //
-  "천상의 날개", //
-  "결속의 체인 플레이트", //
-  "세상을 삼키는 분노", //
-  "영명한 세상의 순환", //
-  "선택이익", //
-  "원시 태동의 대지", //
-  "라이도 : 질서의 창조자", //
-  "새벽을 녹이는 따스함", //
-  "가네샤의 영원한 가호",
-  "지고의 화염 - 이프리트", //
-  "영원히 끝나지 않는 탐구", //
-  "시간을 거스르는 자침", //
-  "천지에 울려퍼지는 포효", //
-  "숙명을 뒤엎는 광란", //
-  "군신의 마지막 갈망", //
-  "영원을 새긴 바다", //
-  "또다른 시간의 흐름", //
-  "플라즈마 초 진공관", //
-  "영원한 나락의 다크버스", //
-  "차원을 관통하는 초신성", //
-  "운명을 거스르는 자",
-  "아린 고통의 비극",
+  "냉정한 지략가의 상의",
+  "승리로 이끄는 정의",
+  "유쾌함 속의 열망",
+  "영면을 위한 준비",
+  "소망을 향한 움직임",
+  "올곧은 자의 이념",
+  "오랜 시간 품어온 소원",
+  "뜨거운 열망의 증표",
+  "무모하지만 합리적인 작전",
+  "승리가 약속된 시간",
+  "영원을 끝낼 오랜 염원",
 ];
 
 import fetch from "node-fetch";
@@ -319,9 +293,9 @@ const DIR_NAME = path.resolve();
 const main = async () => {
   const epicInfos = [];
 
-  for await (const shinwhaItem of all) {
+  for await (const epic of all) {
     const itemInfoResponse = await fetch(
-      `${ENDPOINT}/items?itemName=${encodeURIComponent(shinwhaItem)}`,
+      `${ENDPOINT}/items?itemName=${encodeURIComponent(epic)}`,
       {
         headers: {
           apiKey: API_KEY ?? "",
@@ -341,7 +315,7 @@ const main = async () => {
   }
 
   fs.writeFileSync(
-    path.resolve(DIR_NAME, "public/epicItems.json"),
+    path.resolve(DIR_NAME, "scripts/epicItems.json"),
     JSON.stringify(epicInfos, null, 2)
   );
 };
